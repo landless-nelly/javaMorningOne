554d9fe345ac81e143b4786e8ccb563bff0746a7
landless-nelly <nellyndunge34@gmail.com>
2019-02-26 12:04:35 +0300

    first commit

---
+++ src/dutchessSanny/fileOne.java
@@ -0,0 +1,10 @@
+package dutchessSanny;
+
+public class fileOne {
+    public static void main(String[] args) {
+        System.out.println("HELLO WESTLIFE");
+        System.out.println("I HAVE A DREAM");
+        System.out.println("A SONG TO SING");
+        System.out.println(80-60);
+    }
+}
---
+++ src/dutchessSanny/fileTwo.java
@@ -0,0 +1,7 @@
+package dutchessSanny;
+
+public class fileTwo {
+    public static void main(String[] args) {
+        System.out.println("I am "+20+"years old");
+    }
+}
